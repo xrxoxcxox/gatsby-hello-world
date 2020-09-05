5050e3fd205571f21999d0ff4e8eb544559fc358
xrxoxcxox <watanuki@ju-zen.com>
2020-09-05 23:04:28 +0900

    Data in Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,16 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
